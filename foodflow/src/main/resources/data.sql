b8017733ee2d2b7fb6eabc5dce55834fb26a5aeb
bosnjakmilicaa <128394791+bosnjakmilicaa@users.noreply.github.com>
2025-08-11 09:41:36 +0200

    Update data.sql

--- foodflow/src/main/resources/data.sql
+++ foodflow/src/main/resources/data.sql
@@ -421,6 +421,26 @@ INSERT INTO order_item (id, quantity, order_id, menu_item_version_id) VALUES
 INSERT INTO order_offer (id, order_id, driver_id, status, created_at) VALUES
     (5, 5, 8, 'SENT', NOW());
 
+
+-- KORAK 1: Kreiramo porudžbinu koja je odmah READY_FOR_PICKUP
+INSERT INTO orders (
+    id, status, payment_type, order_type, creation_date, delivery_price, total_price, customer_id, eta
+) VALUES
+    (6, 'READY_FOR_PICKUP', 'CASH', 'REGULAR', NOW(), 180.00, 2200.00, 1, NOW() + INTERVAL '45 minute');
+
+-- KORAK 2: Dodajemo stavku za tu porudžbinu
+INSERT INTO order_item (
+    id, quantity, order_id, menu_item_version_id
+) VALUES
+    (6, 1, 6, 41);
+
+-- KORAK 3: Kreiramo PONUDU za Petra (driver_id = 8) sa statusom 'SENT'
+INSERT INTO order_offer (
+    id, order_id, driver_id, status, created_at
+) VALUES
+    (6, 6, 8, 'SENT', NOW());
+
+
 -- Resetovanje sekvenci
 ALTER SEQUENCE customer_id_seq RESTART WITH 100;
 ALTER SEQUENCE driver_id_seq RESTART WITH 100;
