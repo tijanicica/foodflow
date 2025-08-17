ac20d3471ce3b5a3209b2eeec9576b7864ea9ce1
bosnjakmilicaa <128394791+bosnjakmilicaa@users.noreply.github.com>
2025-08-18 01:04:12 +0200

    AdminUpdate

--- foodflow/src/main/java/com/iis/foodflow/controller/AdminController.java
+++ foodflow/src/main/java/com/iis/foodflow/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.iis.foodflow.controller;
 
 import com.iis.foodflow.dto.request.RegisterManagerRequestDTO;
 import com.iis.foodflow.dto.request.UpdateManagerRequestDTO;
+import com.iis.foodflow.dto.response.AdminDriverPerformanceResponse;
 import com.iis.foodflow.dto.response.ManagerDetailDTO;
 import com.iis.foodflow.dto.response.ManagerInfoDTO;
 import com.iis.foodflow.model.user.Administrator;
@@ -49,5 +50,12 @@ public class AdminController {
     @PreAuthorize("hasAuthority('ROLE_ADMINISTRATOR')")
     public ResponseEntity<ManagerDetailDTO> updateManager(@PathVariable Long id, @RequestBody UpdateManagerRequestDTO request) {
         return ResponseEntity.ok(adminService.updateManager(id, request));
+    }
+
+    @GetMapping("/drivers-performance")
+    @PreAuthorize("hasRole('ADMINISTRATOR')")
+    public ResponseEntity<List<AdminDriverPerformanceResponse>> getAllDriverPerformances() {
+        List<AdminDriverPerformanceResponse> performances = adminService.getAllDriverPerformances();
+        return ResponseEntity.ok(performances);
     }
 }
